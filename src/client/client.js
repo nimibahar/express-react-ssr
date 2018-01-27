5361451e96b444215cbce83f3aef16078e92a703
Nimrod Bachar <nimrodbachar@Nimrods-MacBook-Pro.local>
2018-01-27 20:49:51 +0200

    initial boilerplate setup for react / express ssr apps

---
+++ src/client/client.js
@@ -0,0 +1,8 @@
+// Start up point for the client side application
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import renderer from './helpers/renderer.js';
+
+const app = express();
+
+app.use(express.static('public')); //tells express to deal with the public directory as a static / public resource for
+//the outside world
+
+app.get('/', (req, res) => {
+  res.send(renderer());
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
